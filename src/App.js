f0d1fd5479f9158458246559a01acf052829cfc3
andyhu92 <alreadyusedagain@gmail.com>
2019-10-25 23:47:25 -0700

    Add class properties plugin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends React.Component {
+    state = {
+        count: 0
+    }
     render() {
-        debugger
-        return <h1>Hello Worl!!!d</h1>
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <h2>Count: { this.state.count }</h2>
+                <button onClick={() => this.setState({ count: this.state.count + 1})}>+</button>
+                <button onClick={() => this.setState({ count: this.state.count - 1})}>-</button>
+            </div>
+        )
     }
 }
 
