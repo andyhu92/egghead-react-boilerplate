514e56c5aae1f4ee4498bda794500e3bac8d3d9c
andyhu92 <alreadyusedagain@gmail.com>
2019-11-04 21:44:40 -0800

    Config prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { hot } from 'react-hot-loader/root';
-import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root'
+import React, { Component } from 'react'
 
-const Warning = React.lazy(() => import(/* webpackChunkName: "warning" */ './warning'))
+const Warning = React.lazy(() =>
+  import(/* webpackChunkName: "warning" */ './warning')
+)
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
-    render() {
-        const { count } = this.state
-        return (
-            <div>
-                <h1>Hello World!</h1>
-                <h2 className={count > 10 ? 'warning': ''}>
-                    Count: { count }
-                </h2>
-                <button onClick={() => this.setState({ count: count + 1})}>+</button>
-                <button onClick={() => this.setState({ count: count - 1})}>-</button>
-                { count > 10 &&
-                    <React.Suspense fallback={null}>
-                        <Warning />
-                    </React.Suspense>}
-            </div>
-        )
-    }
+  state = {
+    count: 0
+  }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2 className={count > 10 ? 'warning' : ''}>Count: {count}</h2>
+        <button onClick={() => this.setState({ count: count + 1 })}>+</button>
+        <button onClick={() => this.setState({ count: count - 1 })}>-</button>
+        {count > 10 && (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        )}
+      </div>
+    )
+  }
 }
 
-export default hot(App);
+export default hot(App)
